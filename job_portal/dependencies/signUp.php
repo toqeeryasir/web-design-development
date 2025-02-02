5c30a587b32d6fb2fe6665168680709c664b2745
toqeeryasir <miantoqeeryasir0@gmail.com>
2025-02-02 19:51:14 +0500

    updated code

--- job_portal/dependencies/signUp.php
+++ job_portal/dependencies/signUp.php
@@ -5,7 +5,8 @@ include 'connection.php';
     $username = $_POST['username'];
     $email = $_POST['email'];
     $password = $_POST['password'];
-    $password = [$password];
+
+    # checking if the user already exists or not:
 
     $checkUserQuery = "SELECT * FROM users WHERE email='$email'";
     $result = mysqli_query($conn, $checkUserQuery);
@@ -13,6 +14,8 @@ include 'connection.php';
     if (mysqli_num_rows($result) > 0) {
         echo "User already exists!";
     } else {
+        
+        # insert data if user isn't exists:
 
         $insertQuery = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
         if (mysqli_query($conn, $insertQuery)) {
@@ -21,7 +24,6 @@ include 'connection.php';
             echo "Error: " . $insertQuery . "<br>" . mysqli_error($conn);
         }
     }
-}
 
 mysqli_close($conn);
 ?>
